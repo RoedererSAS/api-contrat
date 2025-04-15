4c9e1a2b46c3345c4253ee83528c604cfd849c3e
Constance de Quatrebarbes c24b <ext.cdequatrebarbes@roederer.fr>
2025-04-15 15:17:53 +0200

    EDIT: /contrats/<id>/ unique endpoint

--- app/main.py
+++ app/main.py
@@ -30,43 +30,6 @@ app = FastAPI(
         "email": "ext.cdequarebarbes@roederer.fr",
     },
 )
-@app.get("/adherent/{id:int}", summary="Consulter les informations d'un assuré",
-    description=f"""## GET ADHERENTCette méthode prend en paramètre un numéro d'assuré principal(entier positif) 
-et renvoie:
-- un code de status 
-- ainsi que les informations associées à l'assuré:
-    - les élément d'identification de l'assuré
-    - les bénéficiaires
-    - les services 
-    - les contrats
-""")    
-def get_adherent(id: Annotated[int, Path(title="Numéro de la personne assurée")]):
-    db_name=os.getenv("CW_AS400_DATABASE")
-    conn = connect_to_as400()
-    if conn:
-        cursor = conn.cursor()
-        pers_id:int = id*100
-        cursor.execute(f"SELECT * FROM {db_name}.DTWADHE AS ADH WHERE ADH.ADHE_PERS_ID = {str(pers_id)}")
-        rows = cursor.fetchall()
-        if rows is None or len(rows) == 0:
-            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=f"Adhérent N°<{id}> not found.")
-        adherents = []
-        contrats = set()
-        parent_ids = set()
-        header:list = ['id', 'cntr_id', 'pers_id', 'date_debut', 'date_fin', 'statut', 'motif_debut', 'motif_fin', 'mod_paiemt', 'freq_paiemt', 'exo_coti', 'parent_id', 'valide_du', 'createur', 'date_ins ', 'date_modif']
-        for row in rows:
-            assure = dict(zip(header, row))
-            # assure["id"] = assure["pers_id"]
-            # del assure["pers_id"]
-            contrats.add(assure["cntr_id"])
-            if assure["parent_id"] is not None:
-                parent_ids.add(assure["parent_id"])
-            del assure["cntr_id"]
-            del assure["parent_id"]
-        assure["contrats"] = [get_contrat(id).get("contrat") for id in list(contrats)]
-        assure["parents"] = list(parent_ids)
-        return {"adherent":assure}
-    return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")    
 
 @app.get("/assures/{id:int}", summary="Consulter les informations d'un assuré",
     description=f"""## GET ASSURE    
@@ -88,32 +51,50 @@ def get_assure(id: Annotated[int, Path(title="Numéro de la personne assurée")]
         pers_id:int = id*100
         cursor.execute(f"SELECT * FROM {db_name}.DTWADHE AS ADH INNER JOIN {db_name}.DTWPERS AS PERS ON ADH.ADHE_PERS_ID = PERS.PERS_ID WHERE PERS.PERS_ID = {str(pers_id)}")
         # AND (ADH.ADHE_DATE_FIN > DATE '{str(Y3_AGO)}' OR ADH.ADHE_DATE_FIN is NULL) AND PERS.PERS_TYPE='AP'
-        row = cursor.fetchone()
-        if row is None or len(row) == 0:
+        rows = cursor.fetchall()
+        if rows is None or len(rows) == 0:
             return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=f"Assuré N°<{id}> not found.")
         header1:list = ['id', 'cntr_id', 'pers_id', 'date_debut', 'date_fin', 'statut', 'motif_debut', 'motif_fin', 'mod_paiemt', 'freq_paiemt', 'exo_coti', 'parent_id', 'valide_du', 'createur', 'date_ins ', 'date_modif']
         header2:list = ["pers_id","numero","matricule","numero_ss","nom","prenom","patronyme","date_naiss","regime","grand_regime","sexe","type","rang_jumeau","situation_fam","teletrans","alert_mail","date_cpte_cli","rue_ligne1","rue_ligne2","rue_ligne3","cp","ville","pays","date_adr","email","tel_fixe","tel_mobile","parent_id","date_creation","aut_prelev","date_ins","date_modif"]
         header:list = header1+header2    
-        assure = dict(zip(header, row))
-        assure["contrats"] = get_contrats(assure["cntr_id"])
-        assure["beneficiaires"] = []
-        assure["services"] = []
+        contrats = set()
+        parent_ids = set()
+        mutuelles = set()
+        entreprises = set()
+        for row in rows:
+            assure = dict(zip(header, row))
+            contrats.add(assure["cntr_id"])
+            
+            if assure["parent_id"] is not None:
+                parent_ids.add(assure["parent_id"])
+            del assure["cntr_id"]
+            del assure["parent_id"]
+        assure["contrats"] = [get_contrat(id).get("contrat") for id in list(contrats)]
+        mutuelles = list(set([c["agent_id"] for c in assure["contrats"]]))
+        entreprises = list(set([c["entr_id"] for c in assure["contrats"]]))
+        produits = list(set([c["prd_id"] for c in assure["contrats"]]))
+        assure["mutuelles"] = [get_mutuelle(m).get("mutuelle") for m in mutuelles]
+        assure["entreprises"] = [get_entreprise(e).get("entreprise") for e in entreprises]
+        assure["produits"] = [get_produit(p).get("produit") for p in produits]
+        assure["categorie"] = assure["contrats"][0]["categorie"]
+        assure["parents"] = list(parent_ids)# je ne sais pas à quoi ca sert pour le moment
+        
         # Assure.model_validate(assure)
         return assure
     return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")
     
 
-@app.get("/entreprises/{id:int}", summary="Consulter les informations d'une entreprise",
-    description=f"""## GET entreprise    
-Cette méthode prend en paramètre un numéro d'adhérent entreprise un entier positif 
-et renvoie:
-un code de status ainsi que les informations associées à l'entreprise
-- les element d'identification de l'entreprise
-- les assurés
-- les bénéficiaires
-- les services 
-- les contrats
-""")
+# @app.get("/entreprises/{id:int}", summary="Consulter les informations d'une entreprise",
+#     description=f"""## GET entreprise    
+# Cette méthode prend en paramètre un numéro d'adhérent entreprise un entier positif 
+# et renvoie:
+# un code de status ainsi que les informations associées à l'entreprise
+# - les element d'identification de l'entreprise
+# - les assurés
+# - les bénéficiaires
+# - les services 
+# - les contrats
+# """)
 def get_entreprise(id:int):
     db_name=os.getenv("CW_AS400_DATABASE")
     conn = connect_to_as400()
@@ -131,7 +112,7 @@ def get_entreprise(id:int):
     return {"status": "ok"}
 
 
-@app.get("/contrats/{id:int}", summary="Consulter les informations d'un ou plusieurs contrats",
+@app.get("{id:int}", summary="Consulter les informations d'un contrat",
     description=f"""## GET CONTRAT
     
 Cette méthode prend en paramètre un numéro de contrat (un entier positif )
@@ -150,23 +131,23 @@ def get_contrat(id:int):
         
         header1 = ["id","entr_id","agen_id","catg_code","prdt_id","date_debut","date_fin","date_suspension","motif_debut","motif_fin","num_charge_cpte","charge_compte","statut","responsable","terme_appel","ordre_decptage","assistance","mode_paiement","impaye_per","impaye_mnt","date_mise_dem","exo_coti","date_modif_erp","date_arrete","date_ins","date_modif"]
         contrat = dict(zip(header1,row))
-        entreprises = get_entreprise(contrat["entr_id"])
-        if entreprises["count"] > 1:
-            contrat["entreprises"]= entreprises["entreprises"]
-        else:
-            contrat["entreprise"]= entreprises["entreprises"][0]
+        contrat["entreprise"]= get_entreprise(contrat["entr_id"]).get("entreprise")
+        del contrat["entr_id"]
         contrat["produit"] = get_produit(contrat["prdt_id"]).get("produit")
+        del contrat["prdt_id"]
         contrat["categorie"] = get_categorie(contrat["catg_code"].strip()).get("categorie")
+        del contrat["catg_code"]
         contrat["mutuelle"] = get_mutuelle(contrat["agen_id"]).get("mutuelle")
+        del contrat["agen_id"]
         return {"contrat": contrat}
     return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")
 
 
-@app.get("/mutuelle/{id:int}", summary="Consulter les informations d'une mutuelle",
-    description=f"""## GET MUTUELLE
-Cette méthode prend en paramètre un code de mutuelle (un entier positif )
-et renvoie:
-un code de status ainsi que la mutuelle""")
+# @app.get("/mutuelle/{id:int}", summary="Consulter les informations d'une mutuelle",
+#     description=f"""## GET MUTUELLE
+# Cette méthode prend en paramètre un code de mutuelle (un entier positif )
+# et renvoie:
+# un code de status ainsi que la mutuelle""")
 def get_mutuelle(id:int):
     db_name=os.getenv("CW_AS400_DATABASE")
     conn = connect_to_as400()
@@ -181,11 +162,11 @@ def get_mutuelle(id:int):
         return {"mutuelle": dict(zip(header1,row))}
     return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")
 
-@app.get("/produit/{id:int}", summary="Consulter les informations d'un produit",
-    description=f"""## GET PRODUIT
-Cette méthode prend en paramètre un code de produit (un entier positif )
-et renvoie:
-un code de status ainsi que le produit""")
+# @app.get("/produit/{id:int}", summary="Consulter les informations d'un produit",
+#     description=f"""## GET PRODUIT
+# Cette méthode prend en paramètre un code de produit (un entier positif )
+# et renvoie:
+# un code de status ainsi que le produit""")
 def get_produit(id:int):
     db_name=os.getenv("CW_AS400_DATABASE")
     conn = connect_to_as400()
@@ -200,11 +181,11 @@ def get_produit(id:int):
         return {"produit": dict(zip(header1,row))}
     return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")
 
-@app.get("/categorie/{code:str}", summary="Consulter les informations d'une catégorie",
-    description=f"""## GET CATEGORIE
-Cette méthode prend en paramètre un code de catégorie (une chaine de caractères)
-et renvoie:
-un code de status ainsi que la catégorie""")
+# @app.get("/categorie/{code:str}", summary="Consulter les informations d'une catégorie",
+#     description=f"""## GET CATEGORIE
+# Cette méthode prend en paramètre un code de catégorie (une chaine de caractères)
+# et renvoie:
+# un code de status ainsi que la catégorie""")
 def get_categorie(code:str):
     db_name=os.getenv("CW_AS400_DATABASE")
     conn = connect_to_as400()
@@ -222,14 +203,14 @@ def get_categorie(code:str):
         return {"categorie": dict(zip(header1,row))}
     return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=f"Erreur de connexion à la BDD")
 
-@app.get("/beneficiaires/{id:int}", summary="Consulter la liste des bénéficiaires",
-    description=f"""## GET BENEFICIAIRE
-Cette méthode prend en paramètre un numéro d'assuré (un entier positif )
-et renvoie:
-un code de status ainsi que la liste des bénéficiaires""")
-def get_beneficiaires(id:int):
-    # Beneficiaire.parse_obj(row)
-    return []
+# @app.get("/beneficiaires/{id:int}", summary="Consulter la liste des bénéficiaires",
+#     description=f"""## GET BENEFICIAIRE
+# Cette méthode prend en paramètre un numéro d'assuré (un entier positif )
+# et renvoie:
+# un code de status ainsi que la liste des bénéficiaires""")
+# def get_beneficiaires(id:int):
+#     # Beneficiaire.parse_obj(row)
+#     return []
 
 @app.get("/healthcheck", status_code=200)
 def healthcheck():
